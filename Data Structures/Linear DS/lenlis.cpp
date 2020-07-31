d323491d44c37021c96684700cbf5ea08b57df52
imjohnzakkam <johnzakkam2592@gmail.com>
2020-07-31 13:32:28 +0530

    u

---
+++ Data Structures/Linear DS/lenlis.cpp
@@ -0,0 +1,85 @@
+#pragma GCC optimize("O3")
+
+#include<bits/stdc++.h>
+#define sp " "
+#define endl "\n"
+#define MAX 10e9
+#define MAXN 100001 
+#define inf_int 2e9
+#define inf_ll 2e18
+#define e 1e-9
+#define vi vector<int>
+#define vl vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define vpl vector<pair<ll,ll>>
+#define vpc vector<pair<char,char>>
+#define adj_list vector<vl> 
+#define umap unordered_map<ll, ll>
+#define pll pair<ll,pair<ll, ll>>
+#define clr(x) memset(x,0,sizeof(x))
+#define set(x,k) memset(x,k,sizeof(x))
+#define cy cout << "YES" << endl
+#define  cn cout << "NO" << endl
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll flag = 0;
+
+ll spf[MAXN]; 
+void sieve() 
+{ 
+    spf[1] = 1; 
+    for (auto i=2; i<MAXN; i++) 
+  		spf[i] = i; 
+  	for (auto i=4; i<MAXN; i+=2) 
+        spf[i] = 2; 
+    for (auto i=3; i*i<MAXN; i++) 
+    {
+        if (spf[i] == i) 
+        { 
+            for (auto j=i*i; j<MAXN; j+=i) 
+                if (spf[j]==j) 
+                    spf[j] = i; 
+        } 
+    } 
+	return;
+} 
+
+vl getFactorization(ll x) 
+{ 
+    vl ret; 
+    while (x != 1) 
+    { 
+        ret.push_back(spf[x]); 
+        x = x / spf[x]; 
+    } 
+    return ret; 
+} 
+
+
+void check()
+{
+	// Determine the length of LIS in array
+	ll n;
+	cin >> n;
+	vl a(n),ans;
+	for(int i=0;i<n;i++) cin >> a[i];
+	ans.push_back(a[0]); // minimum length of LIS will be 1 in any case 
+	for(int i=1;i<n;i++) {
+		if(ans.back()<a[i]) ans.push_back(a[i]); // if the condition satifies we push into the array
+		else ans[lower_bound(ans.begin(),ans.end(),a[i])-ans.begin()] = a[i]; // we put a[i] in the next element which is just >= a[i]
+	}
+	cout << ans.size() << endl;
+    return ;
+}
+
+int32_t main()
+{
+    fastio;
+    ll t = 1;
+    cin >> t;
+    while(t--)
+        check();
+    return 0;
+}
